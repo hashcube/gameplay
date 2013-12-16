5c94e2f964c6f3a85cc46f171950f595d0ccfaa8
Jishnu Mohan <jishnu7@gmail.com>
2013-12-16 12:17:41 +0530

    remove system specific files, add gitignore

--- android/google_play_services_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/debarko/workspace/devkit/addons/gameplay/android/google_play_services_lib/bin/AndroidManifest.xml : \
-/home/debarko/workspace/devkit/addons/gameplay/android/google_play_services_lib/AndroidManifest.xml \
