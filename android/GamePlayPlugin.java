4d98c9a7c9b30876ed07fc110a29c44933cce883
Jishnu Mohan <jishnu7@gmail.com>
2014-03-31 00:01:11 +0530

    code cleanup (indenting)
    * removing mixed indents

--- android/GamePlayPlugin.java
+++ android/GamePlayPlugin.java
@@ -29,270 +29,274 @@ import android.R.id.*;
 
 
 public class GamePlayPlugin implements IPlugin, GameHelper.GameHelperListener {
-	Context _context;
-	Activity _activity;
+  Context _context;
+  Activity _activity;
 
-    // The game helper object. This class is mainly a wrapper around this object.
-    protected GameHelper mHelper;
+  // The game helper object. This class is mainly a wrapper around this object.
+  protected GameHelper mHelper;
 
-    // We expose these constants here because we don't want users of this class
-    // to have to know about GameHelper at all.
-    public static final int CLIENT_GAMES = GameHelper.CLIENT_GAMES;
-    public static final int CLIENT_APPSTATE = GameHelper.CLIENT_APPSTATE;
-    public static final int CLIENT_PLUS = GameHelper.CLIENT_PLUS;
-    public static final int CLIENT_ALL = GameHelper.CLIENT_ALL;
+  // We expose these constants here because we don't want users of this class
+  // to have to know about GameHelper at all.
+  public static final int CLIENT_GAMES = GameHelper.CLIENT_GAMES;
+  public static final int CLIENT_APPSTATE = GameHelper.CLIENT_APPSTATE;
+  public static final int CLIENT_PLUS = GameHelper.CLIENT_PLUS;
+  public static final int CLIENT_ALL = GameHelper.CLIENT_ALL;
 
-    // Requested clients. By default, that's just the games client.
-    protected int mRequestedClients = CLIENT_GAMES;
+  // Requested clients. By default, that's just the games client.
+  protected int mRequestedClients = CLIENT_GAMES;
 
-    // stores any additional scopes.
-    private String[] mAdditionalScopes;
+  // stores any additional scopes.
+  private String[] mAdditionalScopes;
 
-    protected String mDebugTag = "BaseGameActivity";
-    protected boolean mDebugLog = false;
+  protected String mDebugTag = "BaseGameActivity";
+  protected boolean mDebugLog = false;
 
-	public class GPStateEvent extends com.tealeaf.event.Event {
-		String state;
+  public class GPStateEvent extends com.tealeaf.event.Event {
+    String state;
 
-		public GPStateEvent(String state) {
-			super("gameplayLogin");
-			this.state = state;
-		}
-	}
+    public GPStateEvent(String state) {
+      super("gameplayLogin");
+      this.state = state;
+    }
+  }
 
-    /** Constructs a BaseGameActivity with default client (GamesClient). */
-    public GamePlayPlugin() {
-        mHelper = new GameHelper(_activity);
+  /** Constructs a BaseGameActivity with default client (GamesClient). */
+  public GamePlayPlugin() {
+    mHelper = new GameHelper(_activity);
+  }
+
+  /**
+   * Constructs a BaseGameActivity with the requested clients.
+   * @param requestedClients The requested clients (a combination of CLIENT_GAMES,
+   *         CLIENT_PLUS and CLIENT_APPSTATE).
+   */
+  public GamePlayPlugin(int requestedClients) {
+    setRequestedClients(requestedClients);
+  }
+
+  /**
+   * Sets the requested clients. The preferred way to set the requested clients is
+   * via the constructor, but this method is available if for some reason your code
+   * cannot do this in the constructor. This must be called before onCreate in order to
+   * have any effect. If called after onCreate, this method is a no-op.
+   *
+   * @param requestedClients A combination of the flags CLIENT_GAMES, CLIENT_PLUS
+   *         and CLIENT_APPSTATE, or CLIENT_ALL to request all available clients.
+   * @param additionalScopes.  Scopes that should also be requested when the auth
+   *         request is made.
+   */
+  protected void setRequestedClients(int requestedClients, String ... additionalScopes) {
+    mRequestedClients = requestedClients;
+    mAdditionalScopes = additionalScopes;
+  }
+
+  public void onCreateApplication(Context applicationContext) {
+    _context = applicationContext;
+  }
+
+  public void onCreate(Activity activity, Bundle savedInstanceState) {
+    _activity = activity;
+  }
+
+  public void onResume() {
+    mHelper = new GameHelper(_activity);
+    if (mDebugLog) {
+      mHelper.enableDebugLog(mDebugLog, mDebugTag);
+    }
+    mHelper.setup(this, mRequestedClients, mAdditionalScopes);
+  }
+
+  public void onStart() {
+  }
+
+  public void onPause() {
+  }
+
+  @Override
+  public void onSignInFailed(){
+    // Sign in has failed. So show the user the sign-in button.
+    EventQueue.pushEvent(new GPStateEvent("close"));
+  }
+
+  @Override
+  public void onSignInSucceeded(){
+    // show sign-out button, hide the sign-in button
+    EventQueue.pushEvent(new GPStateEvent("open"));
+  }
+
+  public void onStop() {
+    mHelper.onStop();
+  }
+
+  public void onDestroy() {
+  }
+
+  public void onNewIntent(Intent intent) {
+  }
+
+  public void setInstallReferrer(String referrer) {
+  }
+
+  public void onActivityResult(Integer request, Integer result, Intent data) {
+    mHelper.onActivityResult(request, result, data);
+  }
+
+  public boolean consumeOnBackPressed() {
+    return true;
+  }
+
+  public void onBackPressed() {
+  }
+
+  protected GamesClient getGamesClient() {
+    return mHelper.getGamesClient();
+  }
+
+  protected AppStateClient getAppStateClient() {
+    return mHelper.getAppStateClient();
+  }
+
+  protected PlusClient getPlusClient() {
+    return mHelper.getPlusClient();
+  }
+
+  protected boolean isSignedIn() {
+    return mHelper.isSignedIn();
+  }
+
+  public void beginUserInitiatedSignIn(String dummyParam) {
+    mHelper.beginUserInitiatedSignIn(_context);
+  }
+
+  public void signOut(String dummyParam) {
+    mHelper.signOut();
+  }
+
+  protected void showAlert(String title, String message) {
+    mHelper.showAlert(title, message);
+  }
+
+  protected void showAlert(String message) {
+    mHelper.showAlert(message);
+  }
+
+  protected void enableDebugLog(boolean enabled, String tag) {
+    mDebugLog = true;
+    mDebugTag = tag;
+    if (mHelper != null) {
+        mHelper.enableDebugLog(enabled, tag);
+    }
+  }
+
+  protected String getInvitationId() {
+    return mHelper.getInvitationId();
+  }
+
+  protected void reconnectClients(int whichClients) {
+    mHelper.reconnectClients(whichClients);
+  }
+
+  protected String getScopes() {
+    return mHelper.getScopes();
+  }
+
+  protected String[] getScopesArray() {
+    return mHelper.getScopesArray();
+  }
+
+  protected boolean hasSignInError() {
+    return mHelper.hasSignInError();
+  }
+
+  protected GameHelper.SignInFailureReason getSignInError() {
+    return mHelper.getSignInError();
+  }
+
+  public void sendAchievement(String param)
+  {
+    if(!(mHelper.isSignedIn())){
+      return;
     }
 
-    /**
-     * Constructs a BaseGameActivity with the requested clients.
-     * @param requestedClients The requested clients (a combination of CLIENT_GAMES,
-     *         CLIENT_PLUS and CLIENT_APPSTATE).
-     */
-    public GamePlayPlugin(int requestedClients) {
-        setRequestedClients(requestedClients);
-    }
+    logger.log("{gameplay-native} Inside sendAchievement");
+    final Bundle params = new Bundle();
+    String achievementID = "";
+    Float percentSolved = 0F;
 
-    /**
-     * Sets the requested clients. The preferred way to set the requested clients is
-     * via the constructor, but this method is available if for some reason your code
-     * cannot do this in the constructor. This must be called before onCreate in order to
-     * have any effect. If called after onCreate, this method is a no-op.
-     *
-     * @param requestedClients A combination of the flags CLIENT_GAMES, CLIENT_PLUS
-     *         and CLIENT_APPSTATE, or CLIENT_ALL to request all available clients.
-     * @param additionalScopes.  Scopes that should also be requested when the auth
-     *         request is made.
-     */
-    protected void setRequestedClients(int requestedClients, String ... additionalScopes) {
-        mRequestedClients = requestedClients;
-        mAdditionalScopes = additionalScopes;
-    }
-
-	public void onCreateApplication(Context applicationContext) {
-		_context = applicationContext;
-	}
-
-	public void onCreate(Activity activity, Bundle savedInstanceState) {
-		_activity = activity;
-	}
-
-	public void onResume() {
-        mHelper = new GameHelper(_activity);
-        if (mDebugLog) {
-            mHelper.enableDebugLog(mDebugLog, mDebugTag);
+    try {
+      JSONObject ldrData = new JSONObject(param);
+      Iterator<?> keys = ldrData.keys();
+      while( keys.hasNext() ){
+        String key = (String)keys.next();
+        Object o = ldrData.get(key);
+        if(key.equals("achievementID")){
+          achievementID = (String) o;
+          continue;
         }
-        mHelper.setup(this, mRequestedClients, mAdditionalScopes);
-	}
-
-	public void onStart() {
-	}
-
-	public void onPause() {
-	}
-
-	@Override
-	public void onSignInFailed(){
-		// Sign in has failed. So show the user the sign-in button.
-		EventQueue.pushEvent(new GPStateEvent("close"));
-	}
-
-	@Override
-	public void onSignInSucceeded(){
-		// show sign-out button, hide the sign-in button
-	    EventQueue.pushEvent(new GPStateEvent("open"));
-	}
-
-	public void onStop() {
-		mHelper.onStop();
-	}
-
-	public void onDestroy() {
-	}
-
-	public void onNewIntent(Intent intent) {
-	}
-
-	public void setInstallReferrer(String referrer) {
-	}
-
-	public void onActivityResult(Integer request, Integer result, Intent data) {
-		mHelper.onActivityResult(request, result, data);
-	}
-
-	public boolean consumeOnBackPressed() {
-		return true;
-	}
-
-	public void onBackPressed() {
-	}
-
-    protected GamesClient getGamesClient() {
-        return mHelper.getGamesClient();
-    }
-
-    protected AppStateClient getAppStateClient() {
-        return mHelper.getAppStateClient();
-    }
-
-    protected PlusClient getPlusClient() {
-        return mHelper.getPlusClient();
-    }
-
-    protected boolean isSignedIn() {
-        return mHelper.isSignedIn();
-    }
-
-    public void beginUserInitiatedSignIn(String dummyParam) {
-        mHelper.beginUserInitiatedSignIn(_context);
-    }
-
-    public void signOut(String dummyParam) {
-        mHelper.signOut();
-    }
-
-    protected void showAlert(String title, String message) {
-        mHelper.showAlert(title, message);
-    }
-
-    protected void showAlert(String message) {
-        mHelper.showAlert(message);
-    }
-
-    protected void enableDebugLog(boolean enabled, String tag) {
-        mDebugLog = true;
-        mDebugTag = tag;
-        if (mHelper != null) {
-            mHelper.enableDebugLog(enabled, tag);
+        if(key.equals("percentSolved")){
+          percentSolved = new Float(o.toString());
+          continue;
         }
+        params.putString(key, (String) o);
+      }
+    } catch(JSONException e) {
+      logger.log("{gameplay-native} Error in Params of sendAchievement because "+ e.getMessage());
     }
+    mHelper.mGamesClient.unlockAchievement(achievementID);
+  }
 
-    protected String getInvitationId() {
-        return mHelper.getInvitationId();
+  public void showLeaderBoard(String dummyParam)
+  {
+    if(!(mHelper.isSignedIn())){
+      return;
     }
+    //TODO: getlLeaderboardsIndent accepts id as parameter to show
+    //a specific leaderboard.
+    _activity.startActivityForResult(mHelper.mGamesClient.getAllLeaderboardsIntent(), 1);
+  }
 
-    protected void reconnectClients(int whichClients) {
-        mHelper.reconnectClients(whichClients);
+  public void showAchievements(String dummyParam)
+  {
+    if(!(mHelper.isSignedIn())){
+      return;
     }
+    _activity.startActivityForResult(mHelper.mGamesClient.getAchievementsIntent(), 1);
+  }
 
-    protected String getScopes() {
-        return mHelper.getScopes();
+  public void sendScore(String param)
+  {
+    if(!(mHelper.isSignedIn())){
+      return;
     }
+    logger.log("{gameplay-native} Inside sendScore");
 
-    protected String[] getScopesArray() {
-        return mHelper.getScopesArray();
+    final Bundle params = new Bundle();
+    String leaderBoardID = "";
+    Long score = 0L;
+    try {
+      JSONObject ldrData = new JSONObject(param);
+      Iterator<?> keys = ldrData.keys();
+
+      while( keys.hasNext() ){
+        String key = (String)keys.next();
+        Object o = ldrData.get(key);
+        if(key.equals("leaderBoardID")){
+          leaderBoardID = (String) o;
+          continue;
+        }
+        if(key.equals("score")){
+          score =  Long.parseLong(o.toString());
+          continue;
+        }
+        params.putString(key, (String) o);
+      }
+    } catch(JSONException e) {
+      logger.log("{gameplay-native} Error in Params of sendScore because "+ e.getMessage());
     }
+    mHelper.mGamesClient.submitScore(leaderBoardID, score);
+  }
 
-    protected boolean hasSignInError() {
-        return mHelper.hasSignInError();
-    }
-
-    protected GameHelper.SignInFailureReason getSignInError() {
-        return mHelper.getSignInError();
-    }
-
-	public void sendAchievement(String param)
-	{
-		logger.log("{gameplay-native} Inside sendAchievement");
-		if(!(mHelper.isSignedIn())){
-			return;
-		}
-	    final Bundle params = new Bundle();
-	    String achievementID = "";
-	    Float percentSolved = 0F;
-	    try {
-	    	JSONObject ldrData = new JSONObject(param);	
-	        Iterator<?> keys = ldrData.keys();
-	        while( keys.hasNext() ){
-	            String key = (String)keys.next();
-	    		Object o = ldrData.get(key);
-	    		if(key.equals("achievementID")){
-	    			achievementID = (String) o;
-	    			continue;
-	    		}
-	    		if(key.equals("percentSolved")){
-	    			percentSolved = new Float(o.toString());
-	    			continue;
-	    		}
-	    		params.putString(key, (String) o);
-	        }
-		} catch(JSONException e) {
-			logger.log("{gameplay-native} Error in Params of sendAchievement because "+ e.getMessage());
-		}
-		mHelper.mGamesClient.unlockAchievement(achievementID);
-	}
-
-	public void showLeaderBoard(String dummyParam)
-	{
-		if(!(mHelper.isSignedIn())){
-			return;
-		}
-		//TODO: getlLeaderboardsIndent accepts id as parameter to show
-		//a specific leaderboard.
-		_activity.startActivityForResult(mHelper.mGamesClient.getAllLeaderboardsIntent(), 1);
-	}
-
-	public void showAchievements(String dummyParam)
-	{
-		if(!(mHelper.isSignedIn())){
-			return;
-		}
-		_activity.startActivityForResult(mHelper.mGamesClient.getAchievementsIntent(), 1);
-	}
-
-	public void sendScore(String param)
-	{
-		if(!(mHelper.isSignedIn())){
-			return;
-		}
-		logger.log("{gameplay-native} Inside sendScore");
-	    final Bundle params = new Bundle();
-	    String leaderBoardID = "";
-	    Long score = 0L;
-	    try {
-	    	JSONObject ldrData = new JSONObject(param);	
-	        Iterator<?> keys = ldrData.keys();
-	        while( keys.hasNext() ){
-	            String key = (String)keys.next();
-	    		Object o = ldrData.get(key);
-	    		if(key.equals("leaderBoardID")){
-	    			leaderBoardID = (String) o;
-	    			continue;
-	    		}
-	    		if(key.equals("score")){
-	    			score =  Long.parseLong(o.toString());
-	    			continue;
-	    		}
-	    		params.putString(key, (String) o);
-	        }
-		} catch(JSONException e) {
-			logger.log("{gameplay-native} Error in Params of sendScore because "+ e.getMessage());
-		}
-		mHelper.mGamesClient.submitScore(leaderBoardID, score);
-	}
-
-	public void logError(String errorDesc) {
-		logger.log("{gameplay-native} logError "+ errorDesc);
-	}
+  public void logError(String errorDesc) {
+    logger.log("{gameplay-native} logError "+ errorDesc);
+  }
 }
--- js/gameplay.js
+++ js/gameplay.js
@@ -3,115 +3,111 @@ import util.setProperty as setProperty;
 var onSyncUpdate;
 
 function pluginSend(evt, params) {
-	NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent &&
-		NATIVE.plugins.sendEvent("GamePlayPlugin", evt,
-				JSON.stringify(params || {}));
-};
+  NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent &&
+    NATIVE.plugins.sendEvent("GamePlayPlugin", evt, JSON.stringify(params || {}));
+}
 
 function pluginOn(evt, next) {
-	NATIVE && NATIVE.events && NATIVE.events.registerHandler &&
-		NATIVE.events.registerHandler(evt, next);
-};
+  NATIVE && NATIVE.events && NATIVE.events.registerHandler &&
+    NATIVE.events.registerHandler(evt, next);
+}
 
 function invokeCallbacks(list, clear) {
-	// Pop off the first two arguments and keep the rest
-	var args = Array.prototype.slice.call(arguments);
-	args.shift();
-	args.shift();
+  // Pop off the first two arguments and keep the rest
+  var args = Array.prototype.slice.call(arguments);
+  args.shift();
+  args.shift();
 
-	// For each callback,
-	for (var ii = 0; ii < list.length; ++ii) {
-		var next = list[ii];
+  // For each callback,
+  for (var ii = 0; ii < list.length; ++ii) {
+    var next = list[ii];
 
-		// If callback was actually specified,
-		if (next) {
-			// Run it
-			next.apply(null, args);
-		}
-	}
+    // If callback was actually specified,
+    if (next) {
+      // Run it
+      next.apply(null, args);
+    }
+  }
 
-	// If asked to clear the list too,
-	if (clear) {
-		list.length = 0;
-	}
+  // If asked to clear the list too,
+  if (clear) {
+    list.length = 0;
+  }
 }
 
 var Gameplay = Class(function () {
 
-	var loginCB = [];
+  var loginCB = [];
 
-	this.init = function(opts) {
-		logger.log("{gameplay} Registering for events on startup");
-		setProperty(this, "onSyncUpdate", {
-			set: function(f) {
-				//logger.log("Am seting it");
-				// If a callback is being set,
-				if (typeof f === "function") {
-					onSyncUpdate = f;
-				} else {
-					onSyncUpdate = null;
-				}
-			},
-			get: function() {
-				//logger.log("Am getting it");
-				return onSyncUpdate;
-			}
-		});
+  this.init = function(opts) {
+    logger.log("{gameplay} Registering for events on startup");
+    setProperty(this, "onSyncUpdate", {
+      set: function(f) {
+        //logger.log("Am seting it");
+        // If a callback is being set,
+        if (typeof f === "function") {
+          onSyncUpdate = f;
+        } else {
+          onSyncUpdate = null;
+        }
+      },
+      get: function() {
+        //logger.log("Am getting it");
+        return onSyncUpdate;
+      }
+    });
 
-		pluginOn("gameplayLogin", function(evt) {
-			logger.log("{gameplay} State updated:", evt.state);
+    pluginOn("gameplayLogin", function(evt) {
+      logger.log("{gameplay} State updated:", evt.state);
 
-			invokeCallbacks(loginCB, true, evt.state === "open", evt);
-		});
-	};
+      invokeCallbacks(loginCB, true, evt.state === "open", evt);
+    });
+  };
 
-	this.sendAchievement = function(achievementID, percentSolved) {
-		logger.log("{gameplay} Sending of achievement");
+  this.sendAchievement = function(achievementID, percentSolved) {
+    logger.log("{gameplay} Sending of achievement");
 
-		var param = {"achievementID":achievementID,"percentSolved":percentSolved};
+    var param = {"achievementID":achievementID,"percentSolved":percentSolved};
 
-		pluginSend("sendAchievement",param);
-	};
+    pluginSend("sendAchievement",param);
+  };
 
-	this.sendScore = function(leaderBoardID, score) {
-		logger.log("{gameplay} Sending of Score to leaderboard");
+  this.sendScore = function(leaderBoardID, score) {
+    logger.log("{gameplay} Sending of Score to leaderboard");
 
-		var param = {"leaderBoardID":leaderBoardID,"score":score};
+    var param = {"leaderBoardID":leaderBoardID,"score":score};
 
-		pluginSend("sendScore",param);
-	};
+    pluginSend("sendScore",param);
+  };
 
-	this.setNumber = function(name, val) {
-		return;
-	};
+  this.setNumber = function(name, val) {
+    return;
+  };
 
-	this.initSync = function(param_name) {
-		return;
-	};
+  this.initSync = function(param_name) {
+    return;
+  };
 
-	this.logout = function() {
-		logger.log("{gameplay} Logging Out a user");
+  this.logout = function() {
+    logger.log("{gameplay} Logging Out a user");
+    pluginSend("signOut");
+  };
 
-		pluginSend("signOut");
-	};
+  this.login = function(next) {
+    logger.log("{gameplay} Logging in a user");
+    loginCB.push(next);
+    pluginSend("beginUserInitiatedSignIn");
+  };
 
-	this.login = function(next) {
-		logger.log("{gameplay} Logging in a user");
+  this.showLeaderBoard = function() {
+    logger.log("{gameplay} Showing Leaderboard");
+    pluginSend("showLeaderBoard");
+  };
 
-		loginCB.push(next);
-
-		pluginSend("beginUserInitiatedSignIn");
-	};
-
-	this.showLeaderBoard = function() {
-		logger.log("{gameplay} Showing Leaderboard");
-		pluginSend("showLeaderBoard");
-	};
-
-	this.showAchievements = function() {
-		logger.log("{gameplay} Showing Achievements");
-		pluginSend("showAchievements");
-	};
+  this.showAchievements = function() {
+    logger.log("{gameplay} Showing Achievements");
+    pluginSend("showAchievements");
+  };
 });
 
 exports = new Gameplay();
