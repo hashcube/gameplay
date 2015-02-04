80a4d2d7a05b850d3187b8a2e15f1af8a7646e57
Jishnu Mohan <jishnu7@gmail.com>
2015-02-04 20:07:46 +0530

    porting to devkit2

---
+++ js/index.js
@@ -0,0 +1,119 @@
+'use strict';
+/* global Class, exports:true, NATIVE, setProperty, logger */
+
+/* jshint ignore:start */
+import util.setProperty as setProperty;
+/* jshint ignore:end */
+
+function pluginSend(evt, params) {
+  NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent &&
+    NATIVE.plugins.sendEvent('GamePlayPlugin', evt,
+      JSON.stringify(params || {}));
+}
+
+function pluginOn(evt, next) {
+  NATIVE && NATIVE.events && NATIVE.events.registerHandler &&
+    NATIVE.events.registerHandler(evt, next);
+}
+
+function invokeCallbacks(list, clear) {
+  // Pop off the first two arguments and keep the rest
+  var args = Array.prototype.slice.call(arguments),
+    i, item;
+
+  args.shift();
+  args.shift();
+
+  // For each callback,
+  for (i = 0; i < list.length; ++i) {
+    item = list[i];
+
+    // If callback was actually specified,
+    if (item) {
+      // Run it
+      item.apply(null, args);
+    }
+  }
+
+  // If asked to clear the list too,
+  if (clear) {
+    list.length = 0;
+  }
+}
+
+exports = new (Class(function () {
+  var loginCB = [],
+    onSyncUpdate;
+
+  this.init = function () {
+    logger.log('{gameplay} Registering for events on startup');
+    setProperty(this, 'onSyncUpdate', {
+      set: function (f) {
+        // If a callback is being set,
+        if (typeof f === 'function') {
+          onSyncUpdate = f;
+        } else {
+          onSyncUpdate = null;
+        }
+      },
+      get: function () {
+        return onSyncUpdate;
+      }
+    });
+
+    pluginOn('gameplayLogin', function (evt) {
+      logger.log('{gameplay} State updated:', evt.state);
+
+      invokeCallbacks(loginCB, true, evt.state === 'open', evt);
+    });
+  };
+
+  this.sendAchievement = function (achievementID, percentSolved) {
+    var param = {
+      'achievementID': achievementID,
+      'percentSolved': percentSolved
+    };
+
+    logger.log('{gameplay} Sending of achievement');
+    pluginSend('sendAchievement', param);
+  };
+
+  this.sendScore = function (leaderBoardID, score) {
+    var param = {
+      'leaderBoardID': leaderBoardID,
+      'score': score
+    };
+
+    logger.log('{gameplay} Sending of Score to leaderboard');
+    pluginSend('sendScore', param);
+  };
+
+  this.setNumber = function () {
+    return;
+  };
+
+  this.initSync = function () {
+    return;
+  };
+
+  this.logout = function () {
+    logger.log('{gameplay} Logging Out a user');
+    pluginSend('signOut');
+  };
+
+  this.login = function (next) {
+    logger.log('{gameplay} Logging in a user');
+    loginCB.push(next);
+    pluginSend('beginUserInitiatedSignIn');
+  };
+
+  this.showLeaderBoard = function () {
+    logger.log('{gameplay} Showing Leaderboard');
+    pluginSend('showLeaderBoard');
+  };
+
+  this.showAchievements = function () {
+    logger.log('{gameplay} Showing Achievements');
+    pluginSend('showAchievements');
+  };
+}))();
