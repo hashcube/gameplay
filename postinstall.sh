a33e2c63cc2f242446e1ebf1331e31f68bf289ec
Bijosh T J <bijoshtj@gmail.com>
2016-12-22 22:05:29 +0530

    Merging individual modules for googleplay services to single lib project
    - game, base, basement, plus services libs merging together to single play_service_lib

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 android update project -p 'android/BaseGameUtils'
-android update project -p 'android/play-services-base'
-android update project -p 'android/play-services-basement'
-android update project -p 'android/play-services-games'
-android update project -p 'android/play-services-plus'
+android update project -p 'android/google_play_services_lib'
