e516ac7d605d65861bc73396c1587a2517376748
Alex Shcherbyna <alex@Alexs-MacBook-Pro.local>
2018-07-12 16:30:28 +0300

    fixed google services file location

--- build/index.js
+++ build/index.js
@@ -10,7 +10,7 @@ var app_path = app.paths.root;
   } else if (config.target == 'native-android') {
 
       // copy google-services.json from manifest config string `google_services_file`
-      var googleServicesJsonFile = path.join(app_path, app.manifest.google_services_file);
+      var googleServicesJsonFile = path.join(app_path, app.manifest.android.google_services_file);
      return fs.copyAsync(googleServicesJsonFile,
           path.join(app_path, "build",app.manifest.shortName, "app", "google-services.json"));
 
