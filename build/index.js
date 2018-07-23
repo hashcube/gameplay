2107e27379b0113044359b5947f9b49a91ac1f0e
Alex Shcherbyna <alex@Alexs-MacBook-Pro.local>
2018-07-23 14:35:05 +0300

    fixed identation in index.js

--- build/index.js
+++ build/index.js
@@ -6,14 +6,12 @@ exports.onCreateProject = function (api, app, config, cb) {
 var app_path = app.paths.root;
 
   if (config.target == 'native-ios') {
-   return new Promise.resolve(nil)
+    return new Promise.resolve(nil)
   } else if (config.target == 'native-android') {
-
-      // copy google-services.json from manifest config string `google_services_file`
-      var googleServicesJsonFile = path.join(app_path, app.manifest.android.google_services_file);
-     return fs.copyAsync(googleServicesJsonFile,
-          path.join(app_path, "build",app.manifest.shortName, "app", "google-services.json"));
-
+    // copy google-services.json from manifest config string `google_services_file`
+    var googleServicesJsonFile = path.join(app_path, app.manifest.android.google_services_file);
+    return fs.copyAsync(googleServicesJsonFile,
+    path.join(app_path, "build",app.manifest.shortName, "app", "google-services.json"));
   }
   return Promise.resolve(true);
 }
