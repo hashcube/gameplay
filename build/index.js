2283c5e70d4a2b8bd3759fe4cbdf7d8b858e12da
Alex Shcherbyna <alex@Alexs-MacBook-Pro.local>
2018-07-23 14:35:39 +0300

    fixed identation in index.js 2

--- build/index.js
+++ build/index.js
@@ -3,8 +3,7 @@ var path = require('path')
 var fs = Promise.promisifyAll(require('fs-extra'));
 
 exports.onCreateProject = function (api, app, config, cb) {
-var app_path = app.paths.root;
-
+  var app_path = app.paths.root;
   if (config.target == 'native-ios') {
     return new Promise.resolve(nil)
   } else if (config.target == 'native-android') {
